7cd7338ff54d06f530cdbf519ff64a17766033d8
Ryohei Niwase <rniwase@users.noreply.github.com>
2022-01-01 04:28:18 +0000

    specify cable name

--- max10devkitc.py
+++ max10devkitc.py
@@ -82,6 +82,7 @@ def main():
 
     if args.load:
         prog = soc.platform.create_programmer()
+        prog.cable_name = "USB-BlasterII"
         prog.load_bitstream(os.path.join(builder.gateware_dir, soc.build_name + ".sof"))
 
 if __name__ == "__main__":
